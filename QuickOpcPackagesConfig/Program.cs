e44d92bb4b06e794a1e1fd4d27f1c7408c324feb
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2018-09-20 10:06:00 -0400

    Add OpcLabs.QuickOpc via packages.config.

--- QuickOpcPackagesConfig/Program.cs
+++ QuickOpcPackagesConfig/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpcLabs.EasyOpc.DataAccess;
 
 namespace QuickOpcPackagesConfig
 {
@@ -10,6 +6,7 @@ namespace QuickOpcPackagesConfig
     {
         static void Main(string[] args)
         {
+            EasyDAClient.Create();
         }
     }
 }
